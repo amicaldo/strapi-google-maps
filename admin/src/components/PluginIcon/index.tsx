cfc9d92782a0852af4d0b9cb07aa66e80d9380b7
Simon Peters <rodekslav@gmail.com>
2023-06-05 11:29:16 +0200

    corrected dependency typo, colored plugin icon

--- admin/src/components/PluginIcon/index.tsx
+++ admin/src/components/PluginIcon/index.tsx
@@ -5,8 +5,15 @@
  */
 
 import React from 'react';
+import styled from 'styled-components';
 import { PinMap } from '@strapi/icons';
 
-const PluginIcon = () => <PinMap />;
+const StyledPinMap = styled(PinMap)`
+  path {
+    fill: ${({ theme }) => theme.colors.primary500};
+  }
+`;
+
+const PluginIcon = () => <StyledPinMap />;
 
 export default PluginIcon;
