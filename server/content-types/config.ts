5f9159e71f32b2202f0a7dd801e8a3320d4b67ce
Simon Peters <peters@amicaldo.de>
2023-07-11 10:55:25 +0200

    fixed type errors and peer version conflicts

--- server/content-types/config.ts
+++ server/content-types/config.ts
@@ -1,18 +1,21 @@
 import * as utils from '@strapi/utils';
 import { Config } from '../../types';
+import { Model } from '@strapi/utils/dist/types';
 
 const { sanitize } = utils;
 const { contentAPI } = sanitize;
 
-const schema = {
+const schema: Model = {
   kind: 'singleType',
   collectionName: 'google_maps_configs',
   info: {
     singularName: 'config',
     pluralName: 'configs',
+    // @ts-ignore
     displayName: 'Google Maps Config',
   },
   options: {
+    // @ts-ignore
     draftAndPublish: false,
   },
   pluginOptions: {
@@ -36,5 +39,5 @@ const schema = {
 export default schema;
 
 export function sanitizeConfigInput(data: object, ctx: any): Promise<Config> {
-  return contentAPI.input(data, schema, ctx.state.auth);
+  return contentAPI.input(data, schema, ctx.state.auth) as Promise<Config>;
 }
